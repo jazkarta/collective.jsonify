eebc2d7ab5dab388b8a9e3e5a771d6e24c783ef1
Alec Mitchell <alecpm@gmail.com>
2013-11-27 19:32:49 -0800

    Oops.

--- collective/jsonify/wrapper.py
+++ collective/jsonify/wrapper.py
@@ -593,4 +593,5 @@ class Wrapper(dict):
         for meth in ('get_encoding_service_info', 'get_castfire_podcast_info'):
             if hasattr(self._context, meth):
                 values[meth] = getattr(self.context, meth)()
-        self[u'media_info']
+        if values:
+            self[u'media_info'] = values
